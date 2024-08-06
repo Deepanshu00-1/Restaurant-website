fe187f4333c1b8912f0cd3e0fdfb179e09646cfc
Deepanshu <krrohan129@example.com>
2024-08-06 22:25:37 +0530

    creating project

---
+++ RestaurantWebsite/src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+    return(
+        <>
+        <header>
+            <div className="nav-part1"></div>
+            <div className="nav-part2"></div>
+        </header>
+        </>
+    )
+}
+export default Header
